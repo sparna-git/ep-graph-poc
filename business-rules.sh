16ad6c2f10935364cde289e3094dc27873c0c1dd
Thomas Francart <thomas.francart@sparna.fr>
2021-02-02 11:41:00 +0100

    Business rules

--- business-rules.sh
+++ business-rules.sh
@@ -1,11 +1,24 @@
 rules=( 
-	"02-Dataset2_MEP/10-Rules/business-rule-1"
 	"02-Dataset2_MEP/10-Rules/business-rule-2"
+  "02-Dataset2_MEP/10-Rules/business-rule-3"
+	"02-Dataset2_MEP/10-Rules/business-rule-4"
+	"03-Dataset3_Sessions/10-Rules/business-rule-1"
+	"04-Dataset4_Votes/10-Rules/business-rule-1"
+	"04-Dataset4_Votes/10-Rules/business-rule-2"
+	"04-Dataset4_Votes/10-Rules/business-rule-3"
+  "04-Dataset4_Votes/10-Rules/business-rule-4"
+  "04-Dataset4_Votes/10-Rules/business-rule-5"
 )
 
 for ruleDirectory in "${rules[@]}"
 do
   echo "${ruleDirectory}"
-  # do something on $var
-  java -jar shacl-play-app-0.4-onejar.jar validate -s $ruleDirectory/business-rule.ttl -i $ruleDirectory/test-data.ttl -o $ruleDirectory/report.html -o $ruleDirectory/report-raw.html -o $ruleDirectory/report.ttl
+  # do something on $ruleDirectory
+  java -Xms2000M -Xmx4000M -jar shacl-play-app-0.4-onejar.jar validate \
+  	-s $ruleDirectory/business-rule.ttl \
+  	-i $ruleDirectory/test-data.ttl \
+  	-i $ruleDirectory/input \
+  	-o $ruleDirectory/report.html \
+  	-o $ruleDirectory/report-raw.html \
+  	-o $ruleDirectory/report.ttl
 done
