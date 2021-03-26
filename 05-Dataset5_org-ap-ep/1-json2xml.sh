852b581654826db68a69433becdb72c9b83d30a7
Thomas Francart <thomas.francart@sparna.fr>
2021-03-26 11:14:34 +0100

    CV JSON and XML

--- 05-Dataset5_org-ap-ep/1-json2xml.sh
+++ 05-Dataset5_org-ap-ep/1-json2xml.sh
@@ -1,12 +1,2 @@
-export INPUT_FOLDER=$1
-export OUTPUT_FOLDER=$2
-
-for f in $(find $INPUT_FOLDER -name '*.json'); do
-	DIRNAME=$(dirname "${f#*/}")
-	# remove first / from DIRNAME
-	XML_FOLDER=$OUTPUT_FOLDER/${DIRNAME#*/}
-	XML_FILE=$XML_FOLDER/$(basename "$f" | cut -d. -f1).xml
-	echo "Converting $f to $XML_FILE"
-	mkdir --parent $XML_FOLDER
-	python3 json2xml-directory.py $f > $XML_FILE
-done
+./json2xml-directory.sh 01-Data-CV/ 10-XML
+./json2xml-directory.sh 01-Data/ 10-XML
